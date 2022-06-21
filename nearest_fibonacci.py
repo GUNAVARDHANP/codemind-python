dfed8ca42a4c066d03c5e7000d77f1279d91a254
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-06-21 20:34:59 +0530

    l=[0,1] n=int(input()) for i in range(2,n):     l.append(l[i-1]+l[i-2]) b=n f=n+1 while b not in l:     b-=1 while f not in l:     f+=1 if (f-n)<(n-b):     print(f) elif (n-b)<(f-n):     print(b) else:     print(b,f)

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -1,25 +1,16 @@
-n=int(input())
-temp=n
-f=0
-s=1
 l=[0,1]
-while temp:
-   sum=f+s
-   f=s
-   s=sum
-   l.append(sum)
-   temp-=1
-for i in range(n,2*n+1):
-    if i in l:
-        ff=i
-        break
-for i in range(n,-1,-1):
-    if i in l:
-        bf=i
-        break
-if (n-bf)<(ff-n):
-    print(bf)
-elif (ff-n)<(n-bf):
-    print(ff)
+n=int(input())
+for i in range(2,n):
+    l.append(l[i-1]+l[i-2])
+b=n
+f=n+1
+while b not in l:
+    b-=1
+while f not in l:
+    f+=1
+if (f-n)<(n-b):
+    print(f)
+elif (n-b)<(f-n):
+    print(b)
 else:
-    print(bf,ff)
+    print(b,f)
