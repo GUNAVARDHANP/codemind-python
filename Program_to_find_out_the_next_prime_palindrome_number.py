f12d1a728569a29ead9bfb27f0a4ceb479cdcdaf
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-06-21 19:41:02 +0530

    def prime(n):     if n<=1:         return False     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return False         else:             return True def pali(n):     temp=n     rev=0     while temp:         rev=rev*10+(temp%10)         temp//=10     if rev==n:         return True     else:         return False
    n=int(input())
    i=n+1
    while prime(i)==False or pali(i)==False:
        i=i+1
    print(i)

--- Program_to_find_out_the_next_prime_palindrome_number.py
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -1,24 +1,25 @@
-def prime(i):
-    if i==0 or i==1:
+def prime(n):
+    if n<=1:
         return False
     else:
-        for j in range(2,int(i**0.5)+1):
-            if i%j==0:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
                 return False
         else:
             return True
-def palindrome(i):
-    temp=i
+def pali(n):
+    temp=n
     rev=0
-    while temp>0:
+    while temp:
         rev=rev*10+(temp%10)
         temp//=10
-    if rev==i:
+    if rev==n:
         return True
     else:
         return False
+            
 n=int(input())
-for i in range(n+1,(20*n)+1):
-    if prime(i) and palindrome(i):
-        print(i)
-        break
+i=n+1
+while prime(i)==False or pali(i)==False:
+    i=i+1
+print(i)
