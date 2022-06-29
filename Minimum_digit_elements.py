e7797a034caa4359d69bb661e211d0b1e4f0adcc
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-06-29 14:58:53 +0530

    def co(n):     c=0     if n==0:         return 1     if n<0:         n=abs(n)     while n>0:         c+=1         n//=10     return c n=int(input()) l=list(map(int,input().split())) c=0 for i in l:     if co(min(l))==co(i):         c+=1 print(c)

--- Minimum_digit_elements.py
+++ Minimum_digit_elements.py
@@ -1,17 +1,17 @@
-def digcount(n):
-    temp=n
+def co(n):
     c=0
-    while temp>0:
+    if n==0:
+        return 1
+    if n<0:
+        n=abs(n)
+    while n>0:
         c+=1
-        temp//=10
+        n//=10
     return c
 n=int(input())
 l=list(map(int,input().split()))
-m=min(l)
-co=0
-s1=digcount(m)
-for i in range(n):
-    s=digcount(l[i])
-    if s==s1:
-        co+=1
-print(co)
+c=0
+for i in l:
+    if co(min(l))==co(i):
+        c+=1
+print(c)
