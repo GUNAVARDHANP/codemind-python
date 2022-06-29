993492e8293802d64aa5c63c72a93d9ebd27f41c
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-06-29 14:52:34 +0530

    def dig(n):     c=0     if n==0:         c=1     if n<0:         n=abs(n)     while n>0:         c+=1         n//=10     return c n=int(input()) l=list(map(int,input().split())) for i in l:     # print(i)     print(dig(i),end=' ')

--- count_digits.py
+++ count_digits.py
@@ -1,22 +1,15 @@
-def digcnt(n):
+def dig(n):
     c=0
     if n==0:
-        return 1
-    elif n<0:
-       temp=n
-       temp=temp*(-1)
-       while temp:
-           c+=1
-           temp//=10
-       return c
-    else:
-        temp=n
-        while temp:
-            c+=1
-            temp//=10
-        return c
+        c=1
+    if n<0:
+        n=abs(n)
+    while n>0:
+        c+=1
+        n//=10
+    return c
 n=int(input())
 l=list(map(int,input().split()))
-for i in range(n):
-    s=digcnt(l[i])
-    print(s,end=' ')
+for i in l:
+    # print(i)
+    print(dig(i),end=' ')
