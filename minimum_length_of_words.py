f893834a4b9bdd977b52782a04122c35f0b76588
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-06-30 17:31:06 +0530

    n=input() l=[] s=[] for i in n.split():     l.append(i)     x=len(i)     s.append(x) print(min(s))

--- minimum_length_of_words.py
+++ minimum_length_of_words.py
@@ -1,8 +1,8 @@
-a=input()
-m=a.split()
-c=len(m[0])
-for i in range(len(m)):
-    l=len(m[i])
-    if l<c:
-        c=l
-print(c)
+n=input()
+l=[]
+s=[]
+for i in n.split():
+    l.append(i)
+    x=len(i)
+    s.append(x)
+print(min(s))
