782a4133e09b8448c851daa03e51ae7cbb1f0c8a
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-06-30 17:26:12 +0530

    n=input() # n=list(n) c=0 l=[] for i in n.split():     l.append(i)     print(len(l[c]),end=' ')     c+=1

--- characters_in_each_word.py
+++ characters_in_each_word.py
@@ -1,4 +1,9 @@
-a=input()
-m=a.split()
-for i in range(len(m)):
-    print(len(m[i]),end=' ')
+n=input()
+# n=list(n)
+c=0
+l=[]
+for i in n.split():
+    l.append(i)
+    print(len(l[c]),end=' ')
+    c+=1
+        
