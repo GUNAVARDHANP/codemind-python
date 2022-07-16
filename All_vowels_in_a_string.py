1d28dc24e5ca368fcdf98bdf63e93a011eaa32a6
21A91A05H7 <97424252+GUNAVARDHANP@users.noreply.github.com>
2022-07-16 11:32:03 +0530

    a=input() l=[] for i in a:     if i in 'aeiouAEIOU' and i not in l:         l.append(i) l=sorted(l) if 'aeiou' in ''.join(l) or 'AEIOU' in ''.join(l):     print(True) else:     print(False)

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,14 +1,10 @@
-n=input()
-s="aeiou"
+a=input()
 l=[]
-for i in n:
-    if i in s:
+for i in a:
+    if i in 'aeiouAEIOU' and i not in l:
         l.append(i)
-a=set(l)
-if(len(s)==len(a)):
-    print("True")
+l=sorted(l)
+if 'aeiou' in ''.join(l) or 'AEIOU' in ''.join(l):
+    print(True)
 else:
-    print("False")
-
-
-        
+    print(False)
